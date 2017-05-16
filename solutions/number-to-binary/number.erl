e77efee8f1b51d3021eff26861b4c8dfce024c2a
Leonel Mendez <s_mendezl@globalhitss.com>
2017-05-16 16:47:13 -0500

    NEW: Added number solution v2

--- solutions/number-to-binary/number.erl
+++ solutions/number-to-binary/number.erl
@@ -1,12 +1,15 @@
 -module(number).
 -export([number_to_binary/1]).
 
+number_to_binary(1) ->
+	[1];
+
 number_to_binary(Number) ->
 	number_to_binary(Number,[]).
 	
 number_to_binary(0, Acc) ->
-	Acc;
+	lists:reverse(Acc);
 
 number_to_binary(Number, Acc) ->
-	number_to_binary(Number div 2, lists:reverse([ Number rem 2 | Acc])).
+	number_to_binary(Number div 2, [ Number rem 2 | Acc]).
 
