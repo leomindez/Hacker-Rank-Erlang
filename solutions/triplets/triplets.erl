4fc29a29a413de918042928f32bca076f9902551
Leo <leo@MacBook-Pro-de-Leo.local>
2018-05-01 18:42:37 -0500

    NEW: Added triplets solution

--- solutions/triplets/triplets.erl
+++ solutions/triplets/triplets.erl
@@ -1,10 +1,18 @@
-
-
--module(triples).
+-module(triplets).
 -author("leo").
 
 %% API
 -export([compare/2]).
 
 compare(First,Second) ->
-  compare_tripl
+  compare_triplets(First,Second,0,0).
+
+compare_triplets([A|Tail],[B|Tail2],Acc,Acc2) ->
+  if
+    A > B -> compare_triplets(Tail,Tail2,Acc + 1,Acc2);
+    A < B -> compare_triplets(Tail,Tail2,Acc,Acc2 + 1);
+    true -> compare_triplets(Tail,Tail2,Acc,Acc2)
+  end;
+
+compare_triplets([],[],Acc,Acc2) ->
+  {Acc,Acc2}.
